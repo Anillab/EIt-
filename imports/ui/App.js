b4ff531b6f45f2bb53ed9b40adf694a85f919bc9
Anillab <anillabusolo@gmail.com>
2019-10-22 15:28:19 +0000

    authenticate

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,21 +1,33 @@
 import React, { Component } from 'react';
 import  FormEit from './form.jsx'
-import { state } from './form.jsx'
-import Table from './table.jsx'
+import { state } from './form.jsx';
+import Table from './table.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-export default class App extends Component {
+import AccountsUIWrapper from './AccountsUIWrapper.js';
+import {withTracker} from "meteor/react-meteor-data";
+import { Meteor } from 'meteor/meteor';
+class App extends Component {
 
   render(){
     return (
       <div className='App'>
+      <header>
 
       <h1>Welcome to Mest!</h1>
       <h3>Register Below</h3>
+      < AccountsUIWrapper />
+      </header>
           <h1>Eit</h1>
-          <div className='form'>
+
+        {
+          (this.props.currentUser)?
+          (<div className='form'>
           <FormEit />
-          </div>
+          </div>):("")
+
+
+        }
+
           <div className='highlight'>
           <Table />
           </div>
@@ -23,3 +35,8 @@ export default class App extends Component {
     )
   }
 }
+
+export default withTracker(()=>{
+  return {currentUser:Meteor.user()}
+
+})(App);
